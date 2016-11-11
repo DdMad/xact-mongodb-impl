7696027dff0fc90794b9bd90b1bd8a17792b354d
DdMad <ddmad1030@gmail.com>
2016-11-11 21:24:52 +0800

    Fix some errors.

--- src/main/java/Processor.java
+++ src/main/java/Processor.java
@@ -39,15 +39,15 @@ public class Processor {
             try {
                 System.setErr(new PrintStream(System.getProperty("user.dir") + "/d8-xact/" + "result-d8-" + args[1] + ".out"));
                 System.err.println("Total transactions processed: " + count);
-                System.err.println("Total time (second) used: " + duration);
+                System.err.println("Total time (second) used: " + duration/1000);
                 System.err.println("Transaction throughput (xact per second): " + ((double) count)/((double) duration) * 1000);
 
                 System.out.println("Total transactions processed: " + count);
-                System.out.println("Total time (second) used: " + duration);
+                System.out.println("Total time (second) used: " + duration/1000);
                 System.out.println("Transaction throughput (xact per second): " + ((double) count)/((double) duration) * 1000);
                 for (int i = 0; i < 7; i++) {
                     System.out.println((i + 1) + " transactions processed: " + xactCount[i]);
-                    System.out.println("Subtotal time (second) used: " + xactTime[i]);
+                    System.out.println("Subtotal time (second) used: " + xactTime[i]/1000);
                     System.out.println((i + 1) + " transaction throughput (xact per second): " + ((double) xactCount[i])/((double) xactTime[i]) * 1000);
                 }
             } catch (FileNotFoundException e) {
@@ -57,15 +57,15 @@ public class Processor {
             try {
                 System.setErr(new PrintStream(System.getProperty("user.dir") + "/d40-xact/" + "result-d40-" + args[1] + ".out"));
                 System.err.println("Total transactions processed: " + count);
-                System.err.println("Total time (second) used: " + duration);
-                System.err.println("Transaction throughput (xact per second): " + ((double) count)/((double) duration));
+                System.err.println("Total time (second) used: " + duration/1000);
+                System.err.println("Transaction throughput (xact per second): " + ((double) count)/((double) duration) * 1000);
 
                 System.out.println("Total transactions processed: " + count);
-                System.out.println("Total time (second) used: " + duration);
+                System.out.println("Total time (second) used: " + duration/1000);
                 System.out.println("Transaction throughput (xact per second): " + ((double) count)/((double) duration) * 1000);
                 for (int i = 0; i < 7; i++) {
                     System.out.println((i + 1) + " transactions processed: " + xactCount[i]);
-                    System.out.println("Subtotal time (second) used: " + xactTime[i]);
+                    System.out.println("Subtotal time (second) used: " + xactTime[i]/1000);
                     System.out.println((i + 1) + " transaction throughput (xact per second): " + ((double) xactCount[i])/((double) xactTime[i]) * 1000);
                 }
             } catch (FileNotFoundException e) {
